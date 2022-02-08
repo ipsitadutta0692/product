8c561a94fe17e4b270b0c8df7a1f21269325c852
ipsitadutta0692 <99291273+ipsitadutta0692@users.noreply.github.com>
2022-02-09 03:01:35 +0530

    test

---
+++ src/main/java/com/ipsita/product/dtos/TestGitConnectionClass.java
@@ -0,0 +1,4 @@
+package com.ipsita.product.dtos;
+
+public class TestGitConnectionClass {
+}
---
+++ src/test/java/com/ipsita/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipsita.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
